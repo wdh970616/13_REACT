7831ae641f84a306120500e99867f3941377879f
wdh970616 <wdh970616@naver.com>
2024-08-27 11:14:14 +0900

    router

---
+++ 05_router/01_react-router-config/src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// index.js : root 요소를 찾아서 화면을 그려줌
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
---
+++ 05_router/02_navbar/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>준비완료!</h1>
+    </>
+  );
+}
+
+export default App;
